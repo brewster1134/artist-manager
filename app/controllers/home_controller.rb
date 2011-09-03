0217e912bd0ac867cbc0ab92c17087ab32b889d9
Brewster <brewster1134@gmail.com>
2011-09-02 19:04:15 -0700

    bug fixes to splash page and updated schema

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,19 @@ class HomeController < ApplicationController
   include Google
 
   def splash
-    redirect_to home_path unless Settings.splash_page
-    images = Settings.splash_page_featured ? Work.where(:featured => true).collect{|w| w.images} : WorkImage.all
-    @view = (params[:view] || Settings.splash_page_view).to_sym
-    @images = case @view
-    when :slideshow
-      images.shuffle.first(15)
-    when :random
-      images.sample
+    if Settings.splash_page
+      images = Settings.splash_page_featured ? Work.where(:featured => true).collect{|w| w.images} : WorkImage.all
+      @view = (params[:view] || Settings.splash_page_view).to_sym
+      @images = case @view
+      when :slideshow
+        images.shuffle.first(15)
+      when :random
+        images.sample
+      end
+      render :layout => "splash"
+    else
+      redirect_to home_path
     end
-    render :layout => "splash"
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110825230239) do
+ActiveRecord::Schema.define(:version => 20110830021116) do
+
+  create_table "emails", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "series", :force => true do |t|
     t.string   "title"
