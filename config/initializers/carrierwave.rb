e45205c87eec6fe4653f397769b476ade020fbd7
Brewster <brewster1134@gmail.com>
2011-12-20 17:07:27 -0800

    heroku s3 changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,10 @@
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/uploads"
 
-  # google_storage_access_key_id AKA x-goog-project-id
-  # google_storage_secret_access_key AKA Google Cloud Storage ID
   config.fog_credentials = {
-    :provider                         => 'Google',
-    :google_storage_access_key_id     => ENV['GOOGLE_KEY'],
-    :google_storage_secret_access_key => ENV['GOOGLE_SECRET']
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
   }
   config.fog_directory = ENV['FOG_DIRECTORY'] # AKA bucket name
 end
